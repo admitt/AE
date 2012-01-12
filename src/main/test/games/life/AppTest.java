6ebd8e2a4b0c7019be37958383435b337e23175a
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-01-12 11:19:00 +0200

    unit test is updgraded to JUnit 4

---
+++ src/main/test/games/life/AppTest.java
@@ -0,0 +1,11 @@
+package games.life;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: eddy
+ * Date: 1/12/12
+ * Time: 11:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppTest {
+}
