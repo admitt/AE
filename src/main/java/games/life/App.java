8a65bfe72ba7bca576c06c4a10604b714e99c0c9
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-01-12 13:03:10 +0200

    main method is implemeted

--- src/main/java/games/life/App.java
+++ src/main/java/games/life/App.java
@@ -1,13 +1,29 @@
 package games.life;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+    public static void main(final String[] args) throws InterruptedException, IOException {
+        if (args.length != 1) {
+            System.err.print("Please specify initial life");
+            return;
+        }
+
+        int generation = 1;
+        Life life = new Life(readLife(args[0]));
+        while (true) {
+            System.out.println("[" + generation + "]");
+            System.out.println(life);
+            life = life.next();
+            generation++;
+            Thread.sleep(500);
+        }
+    }
+
+    private static String[] readLife(final String fileName) throws IOException {
+        return FileUtils.readLines(new File(fileName)).toArray(new String[0]);
     }
 }
