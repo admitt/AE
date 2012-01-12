7bc3a921f586675427b6f145fd2c8e55c7a90aa4
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-01-12 15:07:24 +0200

    AppTest rename to LifeTest

--- src/test/games/life/LifeTest.java
+++ src/test/games/life/LifeTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class AppTest {
+public class LifeTest {
     private static final String[] LIFE1 = {
             ".*",
             "**"
